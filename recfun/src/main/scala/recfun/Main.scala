478e87d159c955081ec3a38597a953585854b956
David Dunwoody <david.dunwoody@gmail.com>
2014-09-19 01:50:14 +0100

    Working balance function; more tests and fixup required

--- recfun/src/main/scala/recfun/Main.scala
+++ recfun/src/main/scala/recfun/Main.scala
@@ -14,15 +14,28 @@ object Main {
   /**
    * Exercise 1
    */
-  def pascal(c: Int, r: Int): Int = {
+  def pascal(c: Int, r: Int): Int =
     if (c == 0 || c == r) 1
     else pascal(c - 1, r - 1) + pascal(c, r - 1)
-  }
 
   /**
    * Exercise 2
    */
-  def balance(chars: List[Char]): Boolean = ???
+  def balance(chars: List[Char]): Boolean = {
+    def examineChar(unmatched: Int, char: Char): Int = {
+      if (char == '(') unmatched + 1
+      //FIXME: this is dire - need to exit early for unmatchable )
+      else if (char == ')' && unmatched == 0) Int.MinValue
+      else if (char == ')' && unmatched > 0) unmatched - 1
+      else unmatched
+    }
+
+    def inner(depth: Int, chars: List[Char]): Int = {
+      if (chars.isEmpty) depth
+      else inner(examineChar(depth, chars.head), chars.tail)
+    }
+    inner(0, chars) == 0
+  }
 
   /**
    * Exercise 3
